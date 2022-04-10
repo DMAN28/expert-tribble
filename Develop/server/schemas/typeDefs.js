33a047682ad2c63926252c5e53bf57db40f65aab
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-04-10 15:40:48 -0700

    setting up typedefs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -3,7 +3,27 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+type User {
+  _id: ID
+  username: String
+  email: String
+  bookCount: Int
+  savedBooks: [Book]
+  
+}
 
+type book {
+  bookId: ID
+  author: String
+  description: String
+  title: String
+  image: String
+  link: String
+}
+type Query {
+  users: [User]
+  
+}
 `;
 
 // export the typeDefs
