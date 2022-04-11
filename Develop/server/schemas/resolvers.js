40b435b2215a1a4b184ab02d4f5ec944d80c2a7a
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-04-10 21:24:53 -0700

    resolver

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,11 +1,55 @@
-const { User, Book } = require('../models');
+const { User } = require('../models');
+
 
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+      users: async (parent, args, context) =>{
+          if (context.user){
+              const data = User.findOne({ _id: context.user._id })
+              .select('-__v -password')
+            
+    
+            return userData;
+          }
+          }
       }
-    }
-  };
+      },
+
+        Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+        if (context.user) {
+  
+          const user = await User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $push: { thoughts: thought._id } },
+            { new: true }
+          );
+  
+          return user;
+        },
+        
+ 
   
   module.exports = resolvers;
